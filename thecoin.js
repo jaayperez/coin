26d1e6ce14a0b3b21e0e51476288c42794c0cc41
jaayperez <justin@startrev.com>
2021-06-21 02:36:14 -0700

    Verify blockchain integrity

--- thecoin.js
+++ thecoin.js
@@ -42,4 +42,24 @@ class Blockchain {
     newBlock.hash = newBlock.computeHash();
     this.block1chain.push(newBlock);
   }
+
+
+  // This method verifies the integrity of the blockchain.
+  checkChainValidity() {
+    // Checking validity.
+    for (let i = 1; i < this.block1chain.length; i++) {
+      const currentBlock = this.block1chain[i];
+      const nextHash = this.block1chain[i - 1];
+      // Checking current block hash.
+      // If the integrity of the blockchain has been compromised, it returns false.
+      if (currentBlock.hash !== currentBlock.computeHash()) {
+        return false;
+      }
+      // Comparing current block hash with the next block.
+      // Otherwise, in case no anomalies are encountered, it returns true.
+      if (currentBlock.nextHash !== nextHash.hash)
+        return false;
+    }
+    return true;
+  }
 }
